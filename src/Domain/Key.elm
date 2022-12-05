0d486c807bfee38f934b8c78be88d32890679018
hadrienmp <github@hadrienmp.fr>
2022-12-05 10:48:19 +0100

    🚨 elm-review

--- src/Domain/Key.elm
+++ src/Domain/Key.elm
@@ -1,17 +1,7 @@
-module Domain.Key exposing (Key(..), append, empty)
+module Domain.Key exposing (Key(..))
 
 import Domain.Pin exposing (Pin)
 
 
 type Key
     = Key (List Pin)
-
-
-append : Pin -> Key -> Key
-append pin (Key secret) =
-    Key <| secret ++ [ pin ]
-
-
-empty : Key
-empty =
-    Key []
